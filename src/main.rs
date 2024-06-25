38a8ed29afa30816bf98f72b2d7986f61dd7a00b
TheBestWebsite <164817940+TheBestWebsite@users.noreply.github.com>
2024-06-24 20:06:06 -0700

    This is what I am beginning this with.

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+fn main() -> Result<(), std::io::Error> {
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().unwrap();
+    let value = arguments.next().unwrap();
+    println!("Key: {} Value: {}", key, value);
+    write_database(key, value)
+}
+
+struct Database {
+    hashmap: HashMap<String, String>,
+}
+
+impl Database {
+    fn from_disk() -> Database{
+        let contents = std::fs::read_to_string("kv.db");
+        Database {
+            hashmap: HashMap::new(),
+        }
+    }
+}
+
+fn write_database(key: String, value: String) -> Result<(), std::io::Error> {
+    let contents = format!("{}\t{}", key, value);
+    std::fs::write("kv.db", contents)
+}
