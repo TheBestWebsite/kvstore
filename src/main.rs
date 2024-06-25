dd8b9a592413e301230f5a64c39b3aab90045b5a
TheBestWebsite <164817940+TheBestWebsite@users.noreply.github.com>
2024-06-24 20:46:05 -0700

    Useless edit removed.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ fn main() -> Result<(), std::io::Error> {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
-    println!("This is something.");
     println!("Key: {} Value: {}", key, value);
     write_database(key, value)
 }
