077a8329ef20b5344d9b0ca8f4feab771cc228c3
TheBestWebsite <164817940+TheBestWebsite@users.noreply.github.com>
2024-06-25 11:18:05 -0700

    Tutorial finished!
    Now I just need to teach papa.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ fn main() -> Result<(), std::io::Error> {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
-    let db = Database::from_disk()?;
-    db.insert(key, value);
+    let mut db = Database::from_disk()?;
     println!("Key: {} Value: {}", key, value);
-    write_database(key, value)
+    let db_borrow_mut = &mut db;
+    db.insert(key, value);
+    db.flush()?;
+    db_borrow_mut.insert(String::new(), String::new());
+    // write_database(key, value)
+    Ok(())
 }
 
 struct Database {
@@ -21,27 +25,34 @@ impl Database {
         //      Ok(c) => c,
         //      Err(error_value) => return Err(error_value)
         // };
-        let mut hashmap = HashMap::new();
+        let mut hashmap: HashMap<String, String> = HashMap::new();
         for line in contents.lines() {
-            let mut chunks = line.split('\t');
-            let key = chunks.next().unwrap();
-            let value = chunks.next().unwrap();
+            let mut chunks: std::str::Split<char> = line.split('\t');
+            let key: &str = chunks.next().unwrap();
+            let value: &str = chunks.next().unwrap();
             hashmap.insert(key.to_string(), value.to_string());
         }
         Ok(Database {
             hashmap: hashmap,
         })
     }
-    fn insert(mut self, key: String, value: String) {
+    fn insert(&mut self, key: String, value: String) {
         self.hashmap.insert(key, value);
+    }
+    fn flush (&self) -> std::io::Result<()> {
+        let contents: String = todo!("Format the keys and values as a string");
+        std::fs::write("kv.db", contents)
     }
 }
 
-fn write_database(key: String, value: String) -> Result<(), std::io::Error> {
-    let contents = format!("{}\t{}", key, value);
-    std::fs::write("kv.db", contents)
-}
 
-fn foo() {
-    let s = String::new();
-}
+//fn function() {
+  //  let _s3 = foo();
+//}
+
+//fn foo() -> String{
+  //  let s = String::from("Hello, world");
+  //  let s2 = s;
+  //  println!("{}", s2);
+  //  s2
+//}
